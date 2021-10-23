7b06c0bfe2fe8500b79c5add159768ee14887a74
szilajka <szilajka1@gmail.com>
2021-10-23 22:32:34 +0200

    #23 Create sort, paging and filter models.

---
+++ src/ForditasokK.Common/QueryModels/FilterModels/NameAndDateFilterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForditasokK.Common.QueryModels.FilterModels
+{
+    public class NameAndDateFilterModel
+    {
+        [MaxLength(256)]
+        public string Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ src/ForditasokK.Common/QueryModels/PagingModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ForditasokK.Common.QueryModels
+{
+    public class PagingModel
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
