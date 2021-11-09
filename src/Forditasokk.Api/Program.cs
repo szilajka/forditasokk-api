ce911934601e90dc1d4b0b0ca25f7278944343fd
szilajka <szilajka1@gmail.com>
2021-11-09 21:36:46 +0100

    Update projects to .NET 6.

--- src/Forditasokk.Api/Program.cs
+++ src/Forditasokk.Api/Program.cs
@@ -1,20 +1,66 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Forditasokk.Data.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace Forditasokk.Api
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ForditasokkDbContext>(options =>
 {
-    public class Program
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ForditasokkPgsql"));
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Forditasokk.Api", Version = "v1" });
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
     {
-        public static void Main(string[] args)
+        options.IncludeErrorDetails = true;
+        //AuthServer url
+        options.Authority = "https://localhost:44338";
+        options.TokenValidationParameters.ValidAudiences = new List<string>() { "" };
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.SecurityTokenValidators.Add(new JwtSecurityTokenHandler
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            // Disable the built-in JWT claims mapping feature.
+            InboundClaimTypeMap = new Dictionary<string, string>()
+        });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+    });
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy();
+//});
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Forditasokk.Api v1"));
 }
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
